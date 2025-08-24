570f00ac341b0cfe6e03c1aefbf8bc4b5fa127c5
Diaxis <pedro.garcia.holguin@gmail.com>
2025-08-24 19:22:17 +0200

    Añado ejercicio3.java

--- Practica1/Ejercicio3.java
+++ Practica1/Ejercicio3.java
@@ -1,8 +1,23 @@
 package Practica1;
 
+import java.util.Scanner;
 
-//Pasamos a practicar con metodos de la clase math(java.lang)
-//Pedir numero para calcular la raiz, el cuadrado e imprimir resultados
+//En esta practica pedire un numero por consola y dependiendo del numero devolvere la respuesta simple; par/impar
+//Practica con if/else
 public class Ejercicio3 {
-    
+    public static void main(String[]arg){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Me vas a dar un numero y yo te dire si es par o impar: ");
+        int number = sc.nextInt();
+
+        if(number%2==0){
+            System.out.println("El "+number+" es un numero par.");
+        }
+        else{
+            System.out.println("El "+number+" es un numero impar.");
+        }
+        sc.close();
+    }
 }
